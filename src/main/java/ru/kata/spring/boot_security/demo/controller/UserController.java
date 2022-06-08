8a7f0f53e9c12d6ba0814ca17077e099d40e8ed6
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-06-08 16:11:42 +0500

    0.5 Almost Working!

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -23,10 +25,17 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
+    public String show(@PathVariable("id") int id, Model model, HttpServletRequest request) {
+        System.out.println(0);
+        System.out.println(request.getRemoteUser());
+        System.out.println(1);
+        System.out.println(request.getServletPath());
         model.addAttribute("user", userService.getById(id));
         return "users/show";
     }
+
+
+
 
 
 }

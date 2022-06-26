894dfb6aa568a7eebfecf444e738b9bf89de556a
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-06-26 15:21:49 +0500

    0.71 Starting with Index-page

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MainController.java
@@ -0,0 +1,27 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/test")
+public class MainController {
+    UserService userService;
+
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "users/RESTIndex";
+    }
+
+}
